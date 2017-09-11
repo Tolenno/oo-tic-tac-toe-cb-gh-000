08f1ed51a8455e80a194e717932dfa111bd9cc50
Matt Hough <matt@hough.tv>
2017-09-11 11:28:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,8 +97,7 @@ class TicTacToe
       !@board.include?(" ")
     end
 
-    #returns true if the board has not been won but is full, false if the board is not won and the board is not full, and false if the board is won
-    def draw?
+    def draw? #returns true if the board has not been won but is full, false if the board is not won and the board is not full, and false if the board is won
       if won?
         false
       elsif full?
