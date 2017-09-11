d08349c8b6636ca1d2d842bd470fa0aa0f129d45
Matt Hough <matt@hough.tv>
2017-09-11 11:05:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,5 @@
 class TicTacToe
-  def initialize
+  def initialize #initializes the board array to the class
     @board = Array.new(9, " ")
   end
 
@@ -20,5 +20,12 @@ class TicTacToe
 
   ]
 
-
+  def display_board
+    rows = [" #{board[0]} | #{board[1]} | #{board[2]} ", "-----------", " #{board[3]} | #{board[4]} | #{board[5]} ", "-----------", " #{board[6]} | #{board[7]} | #{board[8]} "]
+    puts rows[0]
+    puts rows[1]
+    puts rows[2]
+    puts rows[3]
+    puts rows[4]
+  end
 end
